79efb1a2d478170a9eb08fb69773461a90e4278b
Pritam Chatterjee <pritamchatterjee2009@gmail.com>
2022-05-24 19:37:06 +0530

    Spacing

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
   return (
     <div className="home-page">
       <Navbar className="navbar">
-        <Navbar.Brand>ABC Hospital</Navbar.Brand>
+        <Navbar.Brand>{"    "}ABC Hospital</Navbar.Brand>
         <Nav className="me-auto mb-1">
           <Nav.Link href="">Home</Nav.Link>
           <Nav.Link href="registration">Registration</Nav.Link>
